18ac051f7ce1f20049a70c2671cd24d0fbdf630c
pantherinblack <stupark@bzz.ch>
2023-07-07 14:15:04 +0200

    removed useless models

--- app/src/main/java/ch/pantherinblack/advancedbasicsensors/model/weather/Weather.java
+++ app/src/main/java/ch/pantherinblack/advancedbasicsensors/model/weather/Weather.java
@@ -7,15 +7,11 @@ import java.util.List;
 
 public class Weather {
     CurrentWeather currentWeather;
-    List<Forecast> forecasts;
-    Graph graph;
 
     public void fillFromJSON(JSONObject json) {
         currentWeather = new CurrentWeather();
-        graph = new Graph();
         try {
             currentWeather.fillFromJSON(json.getJSONObject("currentWeather"));
-            graph.fillFromJSON(json.getJSONObject("currentWeather"));
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
@@ -29,19 +25,4 @@ public class Weather {
         this.currentWeather = currentWeather;
     }
 
-    public List<Forecast> getForecasts() {
-        return forecasts;
-    }
-
-    public void setForecasts(List<Forecast> forecasts) {
-        this.forecasts = forecasts;
-    }
-
-    public Graph getGraph() {
-        return graph;
-    }
-
-    public void setGraph(Graph graph) {
-        this.graph = graph;
-    }
 }
