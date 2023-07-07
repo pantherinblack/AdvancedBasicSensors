d48c0642ab0dc796cd1f5f45867f8df1e722492b
pantherinblack <stupark@bzz.ch>
2023-07-07 08:23:44 +0200

    deleted ServiceConnectionAdaption.java

--- app/src/main/java/ch/pantherinblack/advancedbasicsensors/activity/ServiceConnectionAdaption.java
+++
@@ -1,7 +0,0 @@
-package ch.pantherinblack.advancedbasicsensors.activity;
-
-import android.content.ComponentName;
-import android.content.ServiceConnection;
-import android.os.IBinder;
-
-import ch.pantherinblack.advancedbasicsensors.service.ServiceManager;
